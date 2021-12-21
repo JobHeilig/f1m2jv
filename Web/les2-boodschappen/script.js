0c21fb679e5d4de64a3b622c899c8adb50add774
JobHeilig <jobheilig@gmail.com>
2021-12-21 14:45:39 +0100

    koelkast

--- Web/les2-boodschappen/script.js
+++ Web/les2-boodschappen/script.js
@@ -1,1 +1,21 @@
 console.log("Script is geladen!");
+
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById('fruit');
+
+function zetinFruitLa(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTHML = fruit;
+    nieuwElement.className = 'rood';
+    fruitvak.appendChild(nieuwElement);
+}
+
+function zetinGroenteLa(groente){
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTHML = groente;
+    groentevak.appendChild(nieuwElement);
+}
