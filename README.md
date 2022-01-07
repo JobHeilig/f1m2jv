f0ee7aff9c59fd9adabdc88ec22753bcc167392b
JobHeilig <jobheilig@gmail.com>
2022-01-07 14:14:01 +0100

    Opdracht 3 meeste gedeelte af

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Fase 1 Module 2 Museum Online
 ## Javascript
 
-(live view) [http://127.0.0.1:5500/Web/index.html]
+Job Heilig
+
+- [Opdracht 1 - Background Color](file:///C:/MA/Jaar%201/Periode%202/JS/f1m2jv/Web/les1-background-color/index.html)
+- [Opdracht 2 - Boodschappen](file:///C:/MA/Jaar%201/Periode%202/JS/f1m2jv/Web/les2-boodschappen/index.html)
--- Web/les3-tentoonstelling/script.js
+++ Web/les3-tentoonstelling/script.js
@@ -1,5 +1,27 @@
-console.log('hallo wereld')
+const myImage = document.getElementById('myImage');
+const myTitle = document.getElementById('myTitle');
 
-function changeImage(){
-    console.log('plaatje verandert')
+let paintings = [
+    "img/painting0.jpg",
+    "img/painting1.jpg",
+    "img/painting2.jpg",
+    "img/painting3.jpg",
+    "img/painting4.jpg",
+    "img/painting5.jpg"
+];
+
+let titles = [
+    "De schilderijen van van Gogh",
+    "De rode wijngaard",
+    "De sterrennacht",
+    "Sterrennacht boven de rhone",
+    "Zonnebloemen",
+    "Boerderij in de Provence"
+];
+
+
+
+function changeImage(index){
+    myTitle.innerHTML = titles[index];
+    myImage.src = paintings[index];
 }
